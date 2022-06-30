3d24c27e8c8397cba5686871a4fd77eda3c6ed57
Guido <eric5544@gmail.com>
2022-06-30 09:19:02 -0700

    version string added

--- dev/js/smartFilter/sf.js
+++ dev/js/smartFilter/sf.js
@@ -3,3 +3,8 @@ export { SmartFilterCondition } from './SmartFilterCondition.js';
 export { SmartFilterManager } from './SmartFilterManager.js';
 export { SmartFilterConditionType } from './SmartFilter.js';
 export { SmartFilterPropertyType } from './SmartFilter.js';
+
+
+export function getVersion() {
+    return "1.0.0";
+}
--- dev/js/smartFilterUI/sfui.js
+++ dev/js/smartFilterUI/sfui.js
@@ -1,3 +1,9 @@
 export { SmartFilterEditor } from './SmartFilterEditor.js';
 export { SmartFilterManagerUI } from './SmartFilterManagerUI.js';
 export { SmartPropertiesUI } from './SmartPropertiesUI.js';
+
+
+
+export function getVersion() {
+    return "1.0.0";
+}
