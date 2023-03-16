2a04d597b71a2a4da3eb6be20b1d0c8a3f3584a6
Guido <eric5544@gmail.com>
2023-03-16 12:54:33 -0700

    version bump, copy css from UI script

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
+
 module.exports = {
   entry: './dev/js/smartFilter/hcSmartFilter.js',
   mode: "production",
@@ -8,18 +8,4 @@ module.exports = {
     filename: 'smartFilter.min.js',
     library: 'hcSmartFilter', //add this line to enable re-use
   },
-  plugins: [
-    new FileManagerPlugin({
-        events: {
-            onEnd: {
-                copy: [
-                    {
-                        source: path.join(__dirname, 'dev/css/SmartFilterUI.css'),
-                        destination: path.join(__dirname, 'dist/SMartFilterUI.css')
-                    }
-                ]
-            }
-        }
-    })
-]
 };
